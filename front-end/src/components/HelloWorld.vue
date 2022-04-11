1208b1a4dc7161c11015d767db4d489d0847ea4a
ArsenicMeatball <arsenicmeatball@yandex.com>
2022-04-10 23:53:29 -0600

    Properly saves keys in browser store

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -8,69 +8,13 @@ let publicKey = null;
 let privateKey = null;
 const user = "JD";
 const encAlgo = "RSA-OAEP";
+const keyExportFormat = "jwk";
+const hashFunc = "SHA-256";
 let crypto = window.crypto || window.msCrypto;
 if (!crypto.subtle) {
   alert("Cryptography API not Supported");
 }
 let db = null;
-function getDB() {
-  return new Promise(function (resolve) {
-    let indexedDB =
-      window.indexedDB ||
-      window.mozIndexedDB ||
-      window.webkitIndexedDB ||
-      window.msIndexedDB;
-
-    if (!indexedDB) {
-      window.alert(
-        "Your browser doesn't support a stable version of IndexedDB."
-      );
-    }
-    let open = indexedDB.open("SafeSpeechDB", 1);
-
-    open.onerror = function (event) {
-      console.log("error: " + event);
-    };
-
-    open.onsuccess = function (event) {
-      console.log(event);
-      db = open.result;
-      console.log("success: " + db);
-      return resolve();
-    };
-
-    open.onupgradeneeded = function (event) {
-      let db = event.target.result;
-      db.createObjectStore("keys", { keyPath: "id" });
-    };
-  });
-}
-function getKey(name) {
-  return new Promise((resolve) => {
-    let request = db.transaction(["keys"]).objectStore("keys").get(name);
-
-    request.onerror = function (event) {
-      console.log(event);
-      console.log("Unable to retrieve data from database!");
-    };
-
-    request.onsuccess = function (event) {
-      console.log(event);
-      if (request.result) {
-        console.log("Restored key from DB");
-        return resolve(request.result.ar);
-      } else {
-        console.log("Could not retrieve the private key");
-      }
-    };
-  });
-}
-getDB().then(() => {
-  getKey("privateKey").then((privKey) => {
-    privateKey = privKey;
-    console.log(privateKey);
-  });
-});
 
 export default {
   name: "HelloWorld",
@@ -85,19 +29,20 @@ export default {
       console.log(msg);
       if (!privateKey) {
         console.log("No private key, trying to find one");
-        privateKey = await getKey();
+        privateKey = await this.getKey("privateKey");
+        publicKey = await this.getKey("publicKey");
       }
       if (!privateKey) {
         await this.generateKey();
       }
       console.log("ENCRYPTING AND SENDING MESSAGE");
-      const rval = await encrypt(msg, publicKey);
+      const rval = await this.encrypt(msg, publicKey);
       // console.log(rval.iv);
       // console.log(rval.encryptedData);
 
       const encMsgObj = {
-        encryptedMsg: pack(rval.encryptedData),
-        iv: pack(rval.iv),
+        encryptedMsg: this.pack(rval.encryptedData),
+        iv: this.pack(rval.iv),
       };
       console.log(encMsgObj);
       this.$socket.emit("message", encMsgObj);
@@ -109,7 +54,7 @@ export default {
           name: encAlgo,
           modulusLength: 2048,
           publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-          hash: { name: "SHA-256" },
+          hash: { name: hashFunc },
         },
         false,
         ["encrypt", "decrypt"]
@@ -117,39 +62,172 @@ export default {
       console.log(keys);
       publicKey = keys.publicKey;
       privateKey = keys.privateKey;
-
-      console.log("Adding private key to IndexedDB");
-      let request = db
-        .transaction(["keys"], "readwrite")
-        .objectStore("keys")
-        .add({
-          id: "privateKey",
-          ar: privateKey,
-        });
-      request.onsuccess = function (event) {
-        console.log(event);
-        console.log("Added private key to DB");
-      };
-      request.onerror = function (event) {
-        console.log(event);
-        console.log("Could not Add private key to DB");
-      };
-
+      await this.updateKey(publicKey, "publicKey");
+      await this.updateKey(privateKey, "privateKey");
       let pubKeyObj = {
         user: user,
-        key: await crypto.subtle.exportKey("jwk", publicKey),
+        key: await crypto.subtle.exportKey(keyExportFormat, publicKey),
       };
       console.log(pubKeyObj);
       this.$socket.emit("set pubkey", pubKeyObj);
       userPubKeys.set(user, publicKey);
       console.log("FINISHED GENERATING KEY");
     },
+    encode(data) {
+      const encoder = new TextEncoder();
+      return encoder.encode(data);
+    },
+    generateIv() {
+      // https://developer.mozilla.org/en-US/docs/Web/API/AesGcmParams
+      return crypto.getRandomValues(new Uint8Array(16));
+    },
+    async encrypt(data, key) {
+      console.log("ENCRYPT, data, key, encoded, iv, encrypted data, rval");
+      console.log(data);
+      console.log(key);
+      const encoded = this.encode(data);
+      const iv = this.generateIv();
+      // console.log(encoded);
+      // console.log(iv);
+      const encryptedData = await crypto.subtle.encrypt(
+        {
+          name: encAlgo,
+          iv: iv,
+        },
+        key,
+        encoded
+      );
+      return {
+        encryptedData: encryptedData,
+        iv: iv,
+      };
+    },
+    pack(buffer) {
+      return window.btoa(
+        String.fromCharCode.apply(null, new Uint8Array(buffer))
+      );
+    },
+    decode(bytestream) {
+      const decoder = new TextDecoder();
+      return decoder.decode(bytestream);
+    },
+    async decrypt(encryptedData, key, iv) {
+      const encoded = await crypto.subtle.decrypt(
+        {
+          name: encAlgo,
+          iv: iv,
+        },
+        key,
+        encryptedData
+      );
+      return this.decode(encoded);
+    },
+    addKey(key, keyName) {
+      console.log("Adding key " + keyName + " to IndexedDB");
+      let request = db
+        .transaction(["keys"], "readwrite")
+        .objectStore("keys")
+        .add({
+          id: keyName,
+          ar: key,
+        });
+      request.onsuccess = function (event) {
+        console.log(event);
+        console.log("Added key to DB");
+      };
+      request.onerror = function (event) {
+        console.log(event);
+        console.log("Could not add key to DB");
+      };
+    },
+    getDB() {
+      return new Promise(function (resolve) {
+        let indexedDB =
+          window.indexedDB ||
+          window.mozIndexedDB ||
+          window.webkitIndexedDB ||
+          window.msIndexedDB;
+
+        if (!indexedDB) {
+          window.alert(
+            "Your browser doesn't support a stable version of IndexedDB."
+          );
+        }
+        let open = indexedDB.open("SafeSpeechDB", 1);
+
+        open.onerror = function (event) {
+          console.log("error: " + event);
+        };
+
+        open.onsuccess = function () {
+          db = open.result;
+          return resolve();
+        };
+
+        open.onupgradeneeded = function (event) {
+          let db = event.target.result;
+          db.createObjectStore("keys", { keyPath: "id" });
+        };
+      });
+    },
+    getKey(name) {
+      return new Promise((resolve) => {
+        let request = db.transaction(["keys"]).objectStore("keys").get(name);
+        request.onerror = function (event) {
+          console.log(event);
+          console.log("Unable to retrieve data from database!");
+        };
+        request.onsuccess = function (event) {
+          console.log(event);
+          if (request.result) {
+            console.log("Restored key from DB");
+            return resolve(request.result.ar);
+          } else {
+            console.log("Could not retrieve the private key");
+          }
+        };
+      });
+    },
+    removeKey(name) {
+      return new Promise(() => {
+        let request = db
+          .transaction(["keys"], "readwrite")
+          .objectStore("keys")
+          .delete(name);
+        request.onerror = function () {
+          console.log("Unable to remove key from database!");
+        };
+        request.onsuccess = function () {
+          console.log("Removed key");
+        };
+      });
+    },
+    updateKey(newVal, name) {
+      return new Promise(() => {
+        console.log("Updating key");
+        console.log(name);
+        console.log(newVal);
+        let request = db
+          .transaction(["keys"], "readwrite")
+          .objectStore("keys")
+          .put({
+            id: name,
+            ar: newVal,
+          });
+        request.onerror = function () {
+          console.log("Unable to update key in database!");
+        };
+        request.onsuccess = function () {
+          console.log("updated key in indexeddb");
+        };
+      });
+    },
   },
   mounted() {
     this.$socket.on("message", async (obj) => {
       console.log("RECEIVED MSG");
       console.log(obj);
-      const final = await decrypt(
+      const final = await this.decrypt(
         unpack(obj.encryptedMsg),
         privateKey,
         unpack(obj.iv)
@@ -158,14 +236,22 @@ export default {
     });
     this.$socket.on("pubkey", async (obj) => {
       console.log("received pubkey");
-      console.log(obj);
-      publicKey = obj.pubKey;
-      if (publicKey) {
-        console.log("pubkey valid");
+      console.log(obj.pubKey);
+      if (window.publicKey) {
+        console.log("imported public key");
+        publicKey = await crypto.subtle.importKey(
+          keyExportFormat,
+          obj.pubKey,
+          {
+            name: encAlgo,
+            hash: hashFunc,
+          },
+          true,
+          ["encrypt"]
+        );
         console.log(publicKey);
         userPubKeys.set(user, publicKey);
       } else {
-        console.log("no pubkey, generating");
         await this.generateKey();
       }
     });
@@ -173,45 +259,17 @@ export default {
   created() {
     const request = { user: user };
     this.$socket.emit("get pubkey", request);
+    this.getDB().then(() => {
+      this.getKey("publicKey").then((pk) => {
+        publicKey = pk;
+      });
+      this.getKey("privateKey").then((pk) => {
+        privateKey = pk;
+      });
+    });
   },
 };
-
-const encode = (data) => {
-  const encoder = new TextEncoder();
-  return encoder.encode(data);
-};
-const generateIv = () => {
-  // https://developer.mozilla.org/en-US/docs/Web/API/AesGcmParams
-  return crypto.getRandomValues(new Uint8Array(16));
-};
-const encrypt = async (data, key) => {
-  console.log("ENCRYPT, data, key, encoded, iv, encrypted data, rval");
-  console.log(data);
-  console.log(key);
-  const encoded = encode(data);
-  const iv = generateIv();
-  console.log(encoded);
-  console.log(iv);
-  const encryptedData = await crypto.subtle.encrypt(
-    {
-      name: encAlgo,
-      iv: iv,
-    },
-    key,
-    encoded
-  );
-  // console.log(encryptedData);
-  const rval = {
-    encryptedData: encryptedData,
-    iv: iv,
-  };
-  // console.log(rval);
-  return rval;
-};
-const pack = (buffer) => {
-  return window.btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)));
-};
-const unpack = (packed) => {
+function unpack(packed) {
   const string = window.atob(packed);
   const buffer = new ArrayBuffer(string.length);
   const bufferView = new Uint8Array(buffer);
@@ -221,22 +279,7 @@ const unpack = (packed) => {
   }
 
   return buffer;
-};
-const decode = (bytestream) => {
-  const decoder = new TextDecoder();
-  return decoder.decode(bytestream);
-};
-const decrypt = async (encryptedData, key, iv) => {
-  const encoded = await crypto.subtle.decrypt(
-    {
-      name: encAlgo,
-      iv: iv,
-    },
-    key,
-    encryptedData
-  );
-  return decode(encoded);
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
