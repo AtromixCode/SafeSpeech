d8bfd2eaf99ba9b6e26878672ab08dea9d03776f
victor <victor.sanchezalarco@ucalgary.ca>
2022-04-10 20:54:37 -0600

    Added mongo to back-end and .env.example

--- back-end/index.js
+++ back-end/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
@@ -5,9 +6,39 @@ const io = require("socket.io")(http, {
     origin: "*",
   },
 });
+const mongoUser = process.env.MONGO_USER;
+const mongoPass = process.env.MONGO_PASS;
 const port = 8000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cluster0.yiun1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
 
-const {MongoClient} = require('mongodb');
+async function addUser(user) {
+  try {
+    await client.connect();
+
+    const result = await client
+      .db("safe_speech")
+      .collection("users")
+      .insertOne(user);
+
+    console.log(result);
+  } catch (e) {
+    console.log(e.message);
+    if (e.code == 11000) {
+      console.log(
+        "The userName is a unique index, please add handle of error so that the user knows they need another user name"
+      );
+    }
+  } finally {
+    await client.close();
+  }
+}
+
+addUser({
+  userName: "TestUser",
+  password: "Gibberish",
+});
 
 io.on("connection", (socket) => {
   console.log("Connection started");
